4d6d738ab75c00a7558606e0f056f904f8e88b24
ahoang2502 <athoang252@gmail.com>
2024-03-05 12:27:16 +1100

    OAuth (google + github)

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -5,6 +5,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { useTransition } from "react";
+import { useSearchParams } from "next/navigation";
 
 import { CardWrapper } from "./CardWrapper";
 import {
@@ -27,6 +28,12 @@ export const LoginForm = () => {
 	const [error, setError] = useState<string | undefined>("");
 	const [success, setSuccess] = useState<string | undefined>("");
 
+	const searchParams = useSearchParams();
+	const urlError =
+		searchParams.get("error") === "OauthAccountNotLinked"
+			? "Email already in use with different provider!"
+			: "";
+
 	const form = useForm<z.infer<typeof LoginSchema>>({
 		resolver: zodResolver(LoginSchema),
 		defaultValues: {
@@ -41,8 +48,8 @@ export const LoginForm = () => {
 
 		startTransition(() => {
 			login(values).then((data) => {
-				setError(data.error);
-				setSuccess(data.success);
+				setError(data?.error);
+				// setSuccess(data?.success);
 			});
 		});
 	};
@@ -100,7 +107,7 @@ export const LoginForm = () => {
 						/>
 					</div>
 
-					<FormError message={error} />
+					<FormError message={error || urlError} />
 					<FormSuccess message={success} />
 					<Button type="submit" className="w-full " disabled={isPending}>
 						Login
