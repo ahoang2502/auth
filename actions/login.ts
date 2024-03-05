1d33a61a69c1dbb3fe10ef9f94a7cfc9d4876d34
ahoang2502 <athoang252@gmail.com>
2024-03-05 23:00:36 +1100

    verification-token generate + resend

--- actions/login.ts
+++ actions/login.ts
@@ -6,6 +6,8 @@ import { AuthError } from "next-auth";
 import { LoginSchema } from "@/schemas";
 import { signIn } from "@/auth";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
 	const validatedFields = LoginSchema.safeParse(values);
@@ -13,6 +15,18 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
 	if (!validatedFields.success) return { error: "Invalid credentials" };
 
 	const { email, password } = validatedFields.data;
+
+	const existingUser = await getUserByEmail(email);
+	if (!existingUser || !existingUser.email || !existingUser.password)
+		return { error: "Email does not exist!" };
+
+	if (!existingUser.emailVerified) {
+		const verificationToken = await generateVerificationToken(
+			existingUser.email
+		);
+
+		return { success: "Confirmation email sent! Please verify your email before logging in." };
+	}
 
 	try {
 		await signIn("credentials", {
